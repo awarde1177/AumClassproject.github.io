06dc5f565b5b8b6c8a5368e354c585c6513eac8a
awarde1177 <72057774+awarde1177@users.noreply.github.com>
2021-05-01 20:23:38 -0500

    Update student.sql

--- database/student.sql
+++ database/student.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2021 at 07:08 AM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Generation Time: May 02, 2021 at 03:22 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -43,11 +42,11 @@ CREATE TABLE `course_info` (
 --
 
 INSERT INTO `course_info` (`srno`, `c_id`, `c_name`, `c_semester`, `c_instructor`, `c_classroom`, `c_time`) VALUES
-(2, 'd151515', 'Data Structure', 1, 'Ashraf Alattar', 'd135', '1pm-3pm'),
-(3, 'o131313', 'Operating System', 2, 'Hua Yan', 'o181818', '3pm-5pm'),
-(4, 'a565656', 'Algorithm Design & Analysis', 3, 'Ashraf Alattar', 'g565', '1pm-3pm'),
-(5, 'd363636', 'Advance Database', 4, 'Zhimin Gao', 'd125', '5pm-7pm'),
-(6, 'b121212', 'Backend Web Development', 3, 'Zhimin Gao', 'b121', '9am-11am');
+(2, 'd151515', 'Data Structure', 1, 'Professor Hua Yan', 'd135', '1pm-3pm'),
+(3, 'o131313', 'Operating System', 2, 'Professor Hua Yan', 'o181818', '3pm-5pm'),
+(4, 'a565656', 'Algorithm Design & Analysis', 3, 'Professor Belinda', 'g565', '1pm-3pm'),
+(5, 'd363636', 'Advance Database', 4, 'Professor Zhimin Gao', 'd125', '5pm-7pm'),
+(6, 'b121212', 'Backend Web Development', 3, 'Professor Zhimin Gao', 'b121', '9am-11am');
 
 -- --------------------------------------------------------
 
@@ -71,10 +70,8 @@ CREATE TABLE `stucourse` (
 --
 
 INSERT INTO `stucourse` (`data_no`, `stu_id`, `sco_id`, `sco_name`, `sco_sem`, `sco_instructor`, `sco_classroom`, `sco_time`) VALUES
-(7, 'jhon123', 'd151515', 'Data Structure', 1, 'Ashraf Alattar', 'd135', '1pm-3pm'),
-(9, 'jhon123', 'a565656', 'Algorithm Design & Analysis', 3, 'Ashraf Alattar', 'g565', '1pm-3pm'),
-(12, 'test123', 'd151515', 'Data Structure', 1, 'Ashraf Alattar', 'd135', '1pm-3pm'),
-(13, 'test123', 'a565656', 'Algorithm Design & Analysis', 3, 'Ashraf Alattar', 'g565', '1pm-3pm');
+(16, 'a12345', 'a565656', 'Algorithm Design & Analysis', 3, 'Professor Belinda', 'g565', '1pm-3pm'),
+(17, 'a12345', 'd151515', 'Data Structure', 1, 'Professor Hua Yan', 'd135', '1pm-3pm');
 
 -- --------------------------------------------------------
 
@@ -97,8 +94,8 @@ CREATE TABLE `stu_info` (
 --
 
 INSERT INTO `stu_info` (`sr`, `firstname`, `lastname`, `sgender`, `sid`, `semail`, `spassword`) VALUES
-(11, 'jhon', 'cena', 'male', 'jhon123', 'jhon@aum.edi', '12345'),
-(12, 'test', 'test', 'female', 'test123', 'test@aum.edi', 'test123');
+(12, 'test', 'test', 'female', 'test123', 'test@aum.edi', 'test123'),
+(22, 'abhi', 'patel', 'male', 'a12345', 'a@gmail.com', '12345');
 
 --
 -- Indexes for dumped tables
@@ -136,13 +133,13 @@ ALTER TABLE `course_info`
 -- AUTO_INCREMENT for table `stucourse`
 --
 ALTER TABLE `stucourse`
-  MODIFY `data_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `data_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `stu_info`
 --
 ALTER TABLE `stu_info`
-  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
